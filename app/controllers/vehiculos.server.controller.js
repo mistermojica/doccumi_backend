475ffdf649a64982860cd03ee6cbdd0b83c19515
Omar Mojica <omar@codika.net>
2022-05-19 18:10:14 -0400

    Dashboard

--- app/controllers/vehiculos.server.controller.js
+++ app/controllers/vehiculos.server.controller.js
@@ -1,147 +1,278 @@
-var mofac = require('../../config/ModelFactory');
+var mofac = require("../../config/ModelFactory");
 var db = mofac("documi");
 var entityName = "Vehículo(s)";
+let mapEstadoVehiculos = new Map();
 
-exports.list = function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+mapEstadoVehiculos.set("venta", "Vehículos en Venta");
+mapEstadoVehiculos.set("pendiente", "Vehículos Pendientes");
+mapEstadoVehiculos.set("vendido", "Vehículos Vendidos");
+mapEstadoVehiculos.set("taller", "Vehículos en Taller");
 
-    db.Vehiculos.
-    find({}).
-    where("estado").ne("borrado").
-    exec(function(err, data) {
-        if (err) {
-            console.log(__filename + ' >> .list: ' + JSON.stringify(err));
-            res.json({status: "FAILED", message: `Error en la lista de ${entityName}.`, data: {}});
-        }
-        else {
-            console.log("data:", data);
-            res.json({status: "SUCCESS", message: `Lista de ${entityName} generada exitosamente.`, data: data});
-        }
+exports.list = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  db.Vehiculos.find({})
+    .where("estado")
+    .ne("borrado")
+    .exec(function (err, data) {
+      if (err) {
+        console.log(__filename + " >> .list: " + JSON.stringify(err));
+        res.json({
+          status: "FAILED",
+          message: `Error en la lista de ${entityName}.`,
+          data: {},
+        });
+      } else {
+        console.log("data:", data);
+        res.json({
+          status: "SUCCESS",
+          message: `Lista de ${entityName} generada exitosamente.`,
+          data: data,
+        });
+      }
     });
 };
 
-exports.create = function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+exports.create = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
 
-    console.log("req.body:", req.body);
+  console.log("req.body:", req.body);
 
-    var entity = new db.Vehiculos(req.body);
-    entity.save(function(err) {
-        if (err) {
-            console.log(__filename + ' >> .create: ' + JSON.stringify(err));
-            res.json({status: "FAILED", message: `Error en la creación del ${entityName}.`, data: err});
-        }
-        else {
-            res.json({status: "SUCCESS", message: `${entityName} creado exitosamente.`, data: entity});
-        }
-    });
+  var entity = new db.Vehiculos(req.body);
+  entity.save(function (err) {
+    if (err) {
+      console.log(__filename + " >> .create: " + JSON.stringify(err));
+      res.json({
+        status: "FAILED",
+        message: `Error en la creación del ${entityName}.`,
+        data: err,
+      });
+    } else {
+      res.json({
+        status: "SUCCESS",
+        message: `${entityName} creado exitosamente.`,
+        data: entity,
+      });
+    }
+  });
 };
 
-exports.update = function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+exports.update = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
 
-    console.log("req.body:", req.body);
+  console.log("req.body:", req.body);
 
-    db.Vehiculos.
-    findOne({_id: req.body._id}).
-    where("estado").ne("borrado").
-    exec(function(err, entitydb) {
-        if (err) {
-            console.log(__filename + ' >> .update: ' + JSON.stringify(err));
-            res.json({status: "FAILED", message: `Error al obtener ${entityName}.`, data: {}});
-        }
-        else {
-            entitydb.vehNoRegistroPlaca      = req.body.vehNoRegistroPlaca;
-            entitydb.vehChasis               = req.body.vehChasis;
-            entitydb.vehStatusVehiculo       = req.body.vehStatusVehiculo;
-            entitydb.vehTipoEmision          = req.body.vehTipoEmision;
-            entitydb.vehTipoVehiculo         = req.body.vehTipoVehiculo;
-            entitydb.vehAnoFabricacion       = req.body.vehAnoFabricacion;
-            entitydb.vehMarca                = req.body.vehMarca;
-            entitydb.vehModelo               = req.body.vehModelo;
-            entitydb.vehColor                = req.body.vehColor;
-            entitydb.vehPrecio               = req.body.vehPrecio;
-            entitydb.vehCosto                = req.body.vehCosto;
-            entitydb.vehFotoMatricula        = req.body.vehFotoMatricula;
-            entitydb.vehEstado               = req.body.vehEstado;
-            entitydb.vehFechaModificacion    = new Date();
-            entitydb.save(function(err) {
-                if (err) {
-                    console.log(__filename + ' >> .update: ' + JSON.stringify(err));
-                    res.json({status: "FAILED", message: `Error en la actualización de ${entityName}.`, data: {}});
-                }
-                else {
-                    res.json({status: "SUCCESS", message: `${entityName} se actualizó exitosamente.`, data: entitydb});
-                }
+  db.Vehiculos.findOne({ _id: req.body._id })
+    .where("estado")
+    .ne("borrado")
+    .exec(function (err, entitydb) {
+      if (err) {
+        console.log(__filename + " >> .update: " + JSON.stringify(err));
+        res.json({
+          status: "FAILED",
+          message: `Error al obtener ${entityName}.`,
+          data: {},
+        });
+      } else {
+        entitydb.vehNoRegistroPlaca = req.body.vehNoRegistroPlaca;
+        entitydb.vehChasis = req.body.vehChasis;
+        entitydb.vehStatusVehiculo = req.body.vehStatusVehiculo;
+        entitydb.vehTipoEmision = req.body.vehTipoEmision;
+        entitydb.vehTipoVehiculo = req.body.vehTipoVehiculo;
+        entitydb.vehAnoFabricacion = req.body.vehAnoFabricacion;
+        entitydb.vehMarca = req.body.vehMarca;
+        entitydb.vehModelo = req.body.vehModelo;
+        entitydb.vehColor = req.body.vehColor;
+        entitydb.vehPrecio = req.body.vehPrecio;
+        entitydb.vehCosto = req.body.vehCosto;
+        entitydb.vehFotoMatricula = req.body.vehFotoMatricula;
+        entitydb.vehEstado = req.body.vehEstado;
+        entitydb.vehFechaModificacion = new Date();
+        entitydb.save(function (err) {
+          if (err) {
+            console.log(__filename + " >> .update: " + JSON.stringify(err));
+            res.json({
+              status: "FAILED",
+              message: `Error en la actualización de ${entityName}.`,
+              data: {},
             });
-        }
+          } else {
+            res.json({
+              status: "SUCCESS",
+              message: `${entityName} se actualizó exitosamente.`,
+              data: entitydb,
+            });
+          }
+        });
+      }
     });
 };
 
-exports.delete = function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+exports.delete = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
 
-    db.Vehiculos.
-    deleteOne({_id: req.params._id}, function(err){
-        if (err) {
-            console.log(__filename + ' >> .delete: ' + JSON.stringify(err));
-            res.json({status: "FAILED", message: `Error al eliminar ${entityName}.`, data: {}});
-        }
-        else {
-            res.json({status: "SUCCESS", message: `${entityName} se eliminó exitosamente.`, data: {}});
-        }
+  db.Vehiculos.deleteOne({ _id: req.params._id }, function (err) {
+    if (err) {
+      console.log(__filename + " >> .delete: " + JSON.stringify(err));
+      res.json({
+        status: "FAILED",
+        message: `Error al eliminar ${entityName}.`,
+        data: {},
+      });
+    } else {
+      res.json({
+        status: "SUCCESS",
+        message: `${entityName} se eliminó exitosamente.`,
+        data: {},
+      });
+    }
+  });
+};
+
+exports.findById = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  db.Vehiculos.findOne({ _id: req.params.id })
+    .select("-__v")
+    .where("estado")
+    .ne("borrado")
+    .populate({
+      path: "_estado_",
+      select: "codigo nombre -_id",
+      match: { isActive: true },
+    })
+    .exec(function (err, data) {
+      if (err) {
+        console.log(__filename + " >> .findById: " + JSON.stringify(err));
+        res.json({
+          status: "FAILED",
+          message: `Error al obtener el ${entityName}.`,
+          data: {},
+        });
+      } else {
+        res.json({
+          status: "SUCCESS",
+          message: `${entityName} encontrado exitosamente.`,
+          data: data,
+        });
+      }
     });
 };
 
-exports.findById = function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+exports.listpop = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
 
-    db.Vehiculos.
-    findOne({_id: req.params.id}).
-    select("-__v").
-    where("estado").ne("borrado").
-    populate({
-        path: "_estado_", 
-        select: "codigo nombre -_id",
-        match: {isActive: true}
-    }).
-    exec(function(err, data) {
-        if (err) {
-            console.log(__filename + ' >> .findById: ' + JSON.stringify(err));
-            res.json({status: "FAILED", message: `Error al obtener el ${entityName}.`, data: {}});
-        }
-        else {
-            res.json({status: "SUCCESS", message: `${entityName} encontrado exitosamente.`, data: data});
-        }
+  db.Vehiculos.find({})
+    .select("-__v")
+    .where("estado")
+    .ne("borrado")
+    .sort({ orden: 1 })
+    .lean()
+    .populate({
+      path: "_estado_",
+      select: "codigo nombre",
+    })
+    .exec(function (err, data) {
+      if (err) {
+        console.log(__filename + " >> .findById: " + JSON.stringify(err));
+        res.json({
+          status: "FAILED",
+          message: `Error al obtener la lista de ${entityName}.`,
+          data: err,
+        });
+      } else {
+        res.json({
+          status: "SUCCESS",
+          message: `Lista de ${entityName} generada exitosamente.`,
+          data: data,
+        });
+      }
     });
 };
 
-exports.listpop = function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+exports.dashboard = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
 
-    db.Vehiculos.
-    find({}).
-    select("-__v").
-    where("estado").ne("borrado").
-    sort({orden: 1}).
-    lean().
-    populate({
-        path: "_estado_",
-        select: "codigo nombre"
-    }).
-    exec(function(err, data) {
-        if (err) {
-            console.log(__filename + ' >> .findById: ' + JSON.stringify(err));
-            res.json({status: "FAILED", message: `Error al obtener la lista de ${entityName}.`, data: err});
-        }
-        else {
-            res.json({status: "SUCCESS", message: `Lista de ${entityName} generada exitosamente.`, data: data});
-        }
+  console.log("id:", req.params.id);
+
+  db.Vehiculos.find({})
+    .select("-__v")
+    .where("estado")
+    .ne("borrado")
+    .sort({ orden: 1 })
+    .lean()
+    .populate({
+      path: "_estado_",
+      select: "codigo nombre",
+    })
+    .exec(function (err, data) {
+      if (err) {
+        console.log(__filename + " >> .findById: " + JSON.stringify(err));
+        res.json({
+          status: "FAILED",
+          message: `Error al obtener la lista de ${entityName}.`,
+          data: err,
+        });
+      } else {
+        let mapGroup = new Map();
+
+        data.forEach((vehiculo) => {
+          let vehEstado = vehiculo.vehEstado;
+          if (mapGroup.has(vehEstado)) {
+            let grupo = mapGroup.get(vehEstado);
+            grupo.cantidad = grupo.cantidad + 1;
+            grupo.monto = grupo.monto + vehiculo.vehPrecio;
+          } else {
+            let grupo = {
+              cantidad: 0,
+              descripcion: mapEstadoVehiculos.get(vehEstado),
+              monto: 0,
+            };
+            mapGroup.set(vehEstado, grupo);
+          }
+        });
+
+        mapGroup.forEach((value, key) => {
+          console.log(value, key);
+        });
+
+        let result = Array.from(mapGroup.values());
+
+        console.log("result:", result);
+
+        res.json({
+          status: "SUCCESS",
+          message: `Lista de ${entityName} generada exitosamente.`,
+          data: result,
+        });
+      }
     });
 };
--- app/routes/vehiculos.server.routes.js
+++ app/routes/vehiculos.server.routes.js
@@ -1,10 +1,11 @@
-var vehiculos = require('../controllers/vehiculos.server.controller');
+var vehiculos = require("../controllers/vehiculos.server.controller");
 
-module.exports = function(app) {
-    app.route('/vehiculos/list').get(vehiculos.list);
-    app.route('/vehiculos/create').post(vehiculos.create);
-    app.route('/vehiculos/find/:id').get(vehiculos.findById);
-    app.route('/vehiculos/listpop').get(vehiculos.listpop);
-    app.route('/vehiculos/update').post(vehiculos.update);
-    app.route('/vehiculos/delete/:_id').get(vehiculos.delete);
+module.exports = function (app) {
+  app.route("/vehiculos/list").get(vehiculos.list);
+  app.route("/vehiculos/create").post(vehiculos.create);
+  app.route("/vehiculos/find/:id").get(vehiculos.findById);
+  app.route("/vehiculos/listpop").get(vehiculos.listpop);
+  app.route("/vehiculos/update").post(vehiculos.update);
+  app.route("/vehiculos/delete/:_id").get(vehiculos.delete);
+  app.route("/vehiculos/dashboard/:id").get(vehiculos.dashboard);
 };
