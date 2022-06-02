0453c2819c443581cbf431a316b6fcefb7bc04a9
Omar Mojica <omar@codika.net>
2022-06-02 18:44:25 -0400

    Agregando selectores

--- pup-face/fbmp2.js
+++ pup-face/fbmp2.js
@@ -6,7 +6,7 @@ const puppeteer = require('puppeteer'); // v13.0.0 or later
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
-    const timeout = 5000;
+    const timeout = 20000;
     page.setDefaultTimeout(timeout);
 
     async function waitForSelectors(selectors, frame, options) {
@@ -153,7 +153,7 @@ const puppeteer = require('puppeteer'); // v13.0.0 or later
     }
     {
         const targetPage = page;
-        await targetPage.setViewport({"width":1443,"height":1142})
+        await targetPage.setViewport({"width":1600,"height":800})
     }
     {
         const targetPage = page;
@@ -235,11 +235,17 @@ const puppeteer = require('puppeteer'); // v13.0.0 or later
         await scrollIntoViewIfNeeded(element, timeout);
         await element.click({ offset: { x: 163, y: 32.7578125} });
     }
+    // {
+    //     const targetPage = page;
+    //     const element = await waitForSelectors([[".hv4rvrfc:nth-of-type(3) .nhd2j8a9"]], targetPage, { timeout, visible: true });
+    //     await scrollIntoViewIfNeeded(element, timeout);
+    //     await element.click({ offset: { x: 75, y: 70} });
+    // }
     {
-        const targetPage = page;
-        const element = await waitForSelectors([[".hv4rvrfc:nth-of-type(3) .nhd2j8a9"]], targetPage, { timeout, visible: true });
-        await scrollIntoViewIfNeeded(element, timeout);
-        await element.click({ offset: { x: 75, y: 70} });
+      const targetPage = page;
+      const element = await waitForSelectors([["aria/Auto/camioneta"],[".hv4rvrfc:nth-of-type(3) .nhd2j8a9"]], targetPage, { timeout, visible: true });
+      await scrollIntoViewIfNeeded(element, timeout);
+      await element.click({ offset: { x: 98, y: 10.0234375} });
     }
     {
         const targetPage = page;
@@ -247,11 +253,17 @@ const puppeteer = require('puppeteer'); // v13.0.0 or later
         await scrollIntoViewIfNeeded(element, timeout);
         await element.click({ offset: { x: 125, y: 16.7890625} });
     }
+    // {
+    //     const targetPage = page;
+    //     const element = await waitForSelectors([[".hv4rvrfc:nth-of-type(8) .nhd2j8a9"]], targetPage, { timeout, visible: true });
+    //     await scrollIntoViewIfNeeded(element, timeout);
+    //     await element.click({ offset: { x: 89, y: 70} });
+    // }
     {
-        const targetPage = page;
-        const element = await waitForSelectors([[".hv4rvrfc:nth-of-type(8) .nhd2j8a9"]], targetPage, { timeout, visible: true });
-        await scrollIntoViewIfNeeded(element, timeout);
-        await element.click({ offset: { x: 89, y: 70} });
+      const targetPage = page;
+      const element = await waitForSelectors([["aria/2013"],[".hv4rvrfc:nth-of-type(8) .nhd2j8a9"]], targetPage, { timeout, visible: true });
+      await scrollIntoViewIfNeeded(element, timeout);
+      await element.click({ offset: { x: 98, y: 10.0234375} });
     }
     {
         const targetPage = page;
@@ -347,67 +359,79 @@ const puppeteer = require('puppeteer'); // v13.0.0 or later
     }
     {
         const targetPage = page;
-        const element = await waitForSelectors([["#jsc_c_2q > div"]], targetPage, { timeout, visible: true });
+        const element = await waitForSelectors([[".hv4rvrfc:nth-of-type(15) .nhd2j8a9"]], targetPage, { timeout, visible: true });
         await scrollIntoViewIfNeeded(element, timeout);
         await element.click({ offset: { x: 170, y: 31.828125} });
     }
     {
         const targetPage = page;
-        const element = await waitForSelectors([["aria/Hatchback"],["#jsc_c_2o__3"]], targetPage, { timeout, visible: true });
+        const element = await waitForSelectors([["aria/Hatchback"],[".hv4rvrfc:nth-of-type(15) .nhd2j8a9"]], targetPage, { timeout, visible: true });
         await scrollIntoViewIfNeeded(element, timeout);
         await element.click({ offset: { x: 98, y: 10.0234375} });
     }
     {
         const targetPage = page;
-        const element = await waitForSelectors([["aria/El título del vehículo no presenta inconvenientes."],["#mount_0_0_7p > div > div:nth-child(1) > div > div:nth-child(7) > div > div:nth-child(2) > div.rq0escxv.l9j0dhe7.du4w35lb > div > div.j83agx80.cbu4d94t.h3gjbzrl.l9j0dhe7.du4w35lb.qsy8amke > div.rq0escxv.pfnyh3mw.jifvfom9.gs1a9yip.owycx6da.btwxx1t3.j83agx80.buofh1pr.dp1hu0rb.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.j83agx80.cbu4d94t.d2edcug0.hpfvmrgz.pfnyh3mw.dp1hu0rb.rek2kq2y.o36gj0jk.tkr6xdv7.ldhj9swq > div > div.rpm2j7zs.k7i0oixp.gvuykj2m.j83agx80.cbu4d94t.ni8dbmo4.du4w35lb.q5bimw55.ofs802cu.pohlnb88.dkue75c7.mb9wzai9.d8ncny3e.buofh1pr.g5gj957u.tgvbjcpo.l56l04vs.r57mb794.kh7kg01d.eg9m0zos.c3g1iek1.l9j0dhe7.k4xni2cv > div.j83agx80.cbu4d94t.buofh1pr.l9j0dhe7 > div.aov4n071.j83agx80.cbu4d94t.buofh1pr > div > div > div:nth-child(17) > div > label > div > input"]], targetPage, { timeout, visible: true });
+        const element = await waitForSelectors([["aria/El título del vehículo no presenta inconvenientes."],["input[name='title_status']"]], targetPage, { timeout, visible: true });
         await scrollIntoViewIfNeeded(element, timeout);
         await element.click({ offset: { x: 16, y: 13.3515625} });
     }
     {
         const targetPage = page;
-        const element = await waitForSelectors([["#jsc_c_2u > div"]], targetPage, { timeout, visible: true });
+        const element = await waitForSelectors([[".hv4rvrfc:nth-of-type(18) .nhd2j8a9"]], targetPage, { timeout, visible: true });
         await scrollIntoViewIfNeeded(element, timeout);
         await element.click({ offset: { x: 206, y: 36.859375} });
     }
     {
         const targetPage = page;
-        const element = await waitForSelectors([["aria/Bueno"],["#jsc_c_2s__2"]], targetPage, { timeout, visible: true });
+        const element = await waitForSelectors([["aria/Bueno"],[".hv4rvrfc:nth-of-type(18) .nhd2j8a9"]], targetPage, { timeout, visible: true });
         await scrollIntoViewIfNeeded(element, timeout);
         await element.click({ offset: { x: 115, y: 6.015625} });
     }
     {
         const targetPage = page;
-        const element = await waitForSelectors([["#jsc_c_2y > div"]], targetPage, { timeout, visible: true });
+        const element = await waitForSelectors([[".hv4rvrfc:nth-of-type(19) .nhd2j8a9"]], targetPage, { timeout, visible: true });
         await scrollIntoViewIfNeeded(element, timeout);
         await element.click({ offset: { x: 185, y: 32.859375} });
     }
     {
+      const targetPage = page;
+      const element = await waitForSelectors([["aria/Gasolina"],[".hv4rvrfc:nth-of-type(19) .nhd2j8a9"]], targetPage, { timeout, visible: true });
+      await scrollIntoViewIfNeeded(element, timeout);
+      await element.click({ offset: { x: 115, y: 6.015625} });
+    }
+    {
         const targetPage = page;
-        const element = await waitForSelectors([["#jsc_c_2w__2 > div.bp9cbjyn.j83agx80.btwxx1t3.buofh1pr.i1fnvgqd.hpfvmrgz"]], targetPage, { timeout, visible: true });
+        const element = await waitForSelectors([[".hv4rvrfc:nth-of-type(20) .nhd2j8a9"]], targetPage, { timeout, visible: true });
         await scrollIntoViewIfNeeded(element, timeout);
         await element.click({ offset: { x: 71, y: 9.015625} });
     }
     {
-        const targetPage = page;
-        const element = await waitForSelectors([["#jsc_c_32 > div > span"]], targetPage, { timeout, visible: true });
-        await scrollIntoViewIfNeeded(element, timeout);
-        await element.click({ offset: { x: 170, y: 12.859375} });
+      const targetPage = page;
+      const element = await waitForSelectors([["aria/Transmisión automática"],[".hv4rvrfc:nth-of-type(20) .nhd2j8a9"]], targetPage, { timeout, visible: true });
+      await scrollIntoViewIfNeeded(element, timeout);
+      await element.click({ offset: { x: 115, y: 6.015625} });
     }
+    // {
+    //     const targetPage = page;
+    //     const element = await waitForSelectors([["[for='jsc_c_4k']"]], targetPage, { timeout, visible: true });
+    //     await scrollIntoViewIfNeeded(element, timeout);
+    //     await element.click({ offset: { x: 170, y: 12.859375} });
+    // }
     {
         const targetPage = page;
-        const element = await waitForSelectors([["#jsc_c_30__1 > div.bp9cbjyn.j83agx80.btwxx1t3.buofh1pr.i1fnvgqd.hpfvmrgz > div.j83agx80.cbu4d94t.ew0dbk1b.irj2b8pg > div > span"]], targetPage, { timeout, visible: true });
+        const element = await waitForSelectors([[".ieid39z1"]], targetPage, { timeout, visible: true });
         await scrollIntoViewIfNeeded(element, timeout);
         await element.click({ offset: { x: 119, y: 15.71875} });
     }
     {
         const targetPage = page;
-        const element = await waitForSelectors([["aria/Descripción[role=\"textbox\"]"],["#jsc_c_2j"]], targetPage, { timeout, visible: true });
+        const element = await waitForSelectors([["aria/Descripción[role=\"textbox\"]"],[".ieid39z1"]], targetPage, { timeout, visible: true });
         await scrollIntoViewIfNeeded(element, timeout);
         await element.click({ offset: { x: 123, y: 33.8828125} });
     }
     {
         const targetPage = page;
-        const element = await waitForSelectors([["aria/Descripción[role=\"textbox\"]"],["#jsc_c_2j"]], targetPage, { timeout, visible: true });
+        const element = await waitForSelectors([["aria/Descripción[role=\"textbox\"]"],[".ieid39z1"]], targetPage, { timeout, visible: true });
         await scrollIntoViewIfNeeded(element, timeout);
         const type = await element.evaluate(el => el.type);
         if (["textarea","select-one","text","url","tel","search","password","number","email"].includes(type)) {
@@ -423,38 +447,49 @@ const puppeteer = require('puppeteer'); // v13.0.0 or later
     }
     {
         const targetPage = page;
-        const element = await waitForSelectors([["#mount_0_0_7p > div > div:nth-child(1) > div > div:nth-child(7) > div > div:nth-child(2) > div.rq0escxv.l9j0dhe7.du4w35lb > div > div.j83agx80.cbu4d94t.h3gjbzrl.l9j0dhe7.du4w35lb.qsy8amke > div.rq0escxv.pfnyh3mw.jifvfom9.gs1a9yip.owycx6da.btwxx1t3.j83agx80.buofh1pr.dp1hu0rb.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.j83agx80.cbu4d94t.d2edcug0.hpfvmrgz.pfnyh3mw.dp1hu0rb.rek2kq2y.o36gj0jk.tkr6xdv7.ldhj9swq > div > div.rpm2j7zs.k7i0oixp.gvuykj2m.j83agx80.cbu4d94t.ni8dbmo4.du4w35lb.q5bimw55.ofs802cu.pohlnb88.dkue75c7.mb9wzai9.d8ncny3e.buofh1pr.g5gj957u.tgvbjcpo.l56l04vs.r57mb794.kh7kg01d.eg9m0zos.c3g1iek1.l9j0dhe7.k4xni2cv > div.j83agx80.cbu4d94t.buofh1pr.l9j0dhe7 > div.aov4n071.j83agx80.cbu4d94t.buofh1pr > div > div > div.aahdfvyu.hv4rvrfc.dati1w0a > div.bi6gxh9e.aov4n071.l9j0dhe7 > div > div > div > div > div > div:nth-child(1) > div > i"]], targetPage, { timeout, visible: true });
+        const element = await waitForSelectors([[".dati1w0a .aov4n071 [role]"]], targetPage, { timeout, visible: true });
         await scrollIntoViewIfNeeded(element, timeout);
         await element.click({ offset: { x: 9, y: 4.7578125} });
     }
+    const path = require('path');
+
+    
     {
         const targetPage = page;
-        const element = await waitForSelectors([["#mount_0_0_7p > div > div:nth-child(1) > div > div:nth-child(7) > div > div:nth-child(2) > div.rq0escxv.l9j0dhe7.du4w35lb > div > div.j83agx80.cbu4d94t.h3gjbzrl.l9j0dhe7.du4w35lb.qsy8amke > div.rq0escxv.pfnyh3mw.jifvfom9.gs1a9yip.owycx6da.btwxx1t3.j83agx80.buofh1pr.dp1hu0rb.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.j83agx80.cbu4d94t.d2edcug0.hpfvmrgz.pfnyh3mw.dp1hu0rb.rek2kq2y.o36gj0jk.tkr6xdv7.ldhj9swq > div > div.rpm2j7zs.k7i0oixp.gvuykj2m.j83agx80.cbu4d94t.ni8dbmo4.du4w35lb.q5bimw55.ofs802cu.pohlnb88.dkue75c7.mb9wzai9.d8ncny3e.buofh1pr.g5gj957u.tgvbjcpo.l56l04vs.r57mb794.kh7kg01d.eg9m0zos.c3g1iek1.l9j0dhe7.k4xni2cv > div.j83agx80.cbu4d94t.buofh1pr.l9j0dhe7 > div.aov4n071.j83agx80.cbu4d94t.buofh1pr > div > div > div.aahdfvyu.hv4rvrfc.dati1w0a > label:nth-child(2) > input"]], targetPage, { timeout, visible: true });
-        await scrollIntoViewIfNeeded(element, timeout);
-        const type = await element.evaluate(el => el.type);
-        if (["textarea","select-one","text","url","tel","search","password","number","email"].includes(type)) {
-          await element.type("C:\\fakepath\\Dimensiones Mobiles.png");
-        } else {
-          await element.focus();
-          await element.evaluate((el, value) => {
-            el.value = value;
-            el.dispatchEvent(new Event('input', { bubbles: true }));
-            el.dispatchEvent(new Event('change', { bubbles: true }));
-          }, "C:\\fakepath\\Dimensiones Mobiles.png");
-        }
+
+        const filepath = '/Users/omarmojica/proyectos/documi/documibackend/public/uploads/amadita.png';
+        const filePath = path.relative(filepath);
+      // const filePath = path.relative(process.cwd(), __dirname + filepath);
+      const element = await waitForSelectors([[".dati1w0a .aov4n071 [role]"]], targetPage, { timeout, visible: true });
+      await scrollIntoViewIfNeeded(element, timeout);
+
+      await element.uploadFile(filePath);
+      await element.evaluate(upload => upload.dispatchEvent(new Event('change', { bubbles: true })));
+
+        // const type = await element.evaluate(el => el.type);
+        // if (["textarea","select-one","text","url","tel","search","password","number","email"].includes(type)) {
+        //   await element.type("/Users/omarmojica/proyectos/documi/documibackend/public/uploads/amadita.png");
+        // } else {
+        //   await element.focus();
+        //   await element.evaluate((el, value) => {
+        //     el.value = value;
+        //     el.dispatchEvent(new Event('input', { bubbles: true }));
+        //     el.dispatchEvent(new Event('change', { bubbles: true }));
+        //   }, "/Users/omarmojica/proyectos/documi/documibackend/public/uploads/amadita.png");
+        // }
     }
     {
         const targetPage = page;
-        const element = await waitForSelectors([["#mount_0_0_7p > div > div:nth-child(1) > div > div:nth-child(7) > div > div:nth-child(2) > div.rq0escxv.l9j0dhe7.du4w35lb > div > div.j83agx80.cbu4d94t.h3gjbzrl.l9j0dhe7.du4w35lb.qsy8amke > div.rq0escxv.pfnyh3mw.jifvfom9.gs1a9yip.owycx6da.btwxx1t3.j83agx80.buofh1pr.dp1hu0rb.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.j83agx80.cbu4d94t.d2edcug0.hpfvmrgz.pfnyh3mw.dp1hu0rb.rek2kq2y.o36gj0jk.tkr6xdv7.ldhj9swq > div > div.dati1w0a.ihqw7lf3.hv4rvrfc.discj3wi.taijpn5t.pfnyh3mw.j83agx80.l6v480f0.bp9cbjyn > div > div > div > div > div.bp9cbjyn.j83agx80.taijpn5t.c4xchbtz.by2jbhx6.a0jftqn4 > div > span > span"]], targetPage, { timeout, visible: true });
+        const element = await waitForSelectors([[".s1i5eluu.qypqp5cg"]], targetPage, { timeout, visible: true });
         await scrollIntoViewIfNeeded(element, timeout);
         await element.click({ offset: { x: 66.421875, y: 7} });
     }
     {
         const targetPage = page;
-        const element = await waitForSelectors([["#mount_0_0_7p > div > div:nth-child(1) > div > div:nth-child(7) > div > div:nth-child(2) > div.rq0escxv.l9j0dhe7.du4w35lb > div > div.j83agx80.cbu4d94t.h3gjbzrl.l9j0dhe7.du4w35lb.qsy8amke > div.rq0escxv.pfnyh3mw.jifvfom9.gs1a9yip.owycx6da.btwxx1t3.j83agx80.buofh1pr.dp1hu0rb.l9j0dhe7.du4w35lb > div.rq0escxv.l9j0dhe7.j83agx80.cbu4d94t.d2edcug0.hpfvmrgz.pfnyh3mw.dp1hu0rb.rek2kq2y.o36gj0jk.tkr6xdv7.ldhj9swq > div > div.dati1w0a.ihqw7lf3.hv4rvrfc.discj3wi.taijpn5t.pfnyh3mw.j83agx80.l6v480f0.bp9cbjyn > div:nth-child(2) > div > div > div.bp9cbjyn.j83agx80.taijpn5t.c4xchbtz.by2jbhx6.a0jftqn4 > div > span > span"]], targetPage, { timeout, visible: true });
+        const element = await waitForSelectors([[".buofh1pr.rj1gh0hx"]], targetPage, { timeout, visible: true });
         await scrollIntoViewIfNeeded(element, timeout);
         await element.click({ offset: { x: 31.515625, y: 13} });
     }
 
-    await browser.close();
+    // await browser.close();
 })();
