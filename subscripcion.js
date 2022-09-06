4735e79ec62a05f302cc809da7647fd8ed08c7e6
Omar Mojica <omar@codika.net>
2022-09-06 14:46:47 -0400

    Stripe 6

--- subscripcion.js
+++ subscripcion.js
@@ -299,7 +299,7 @@ app.get('/subscriptions', async (req, res) => {
 
   const subscriptions = await stripe.subscriptions.list({
     customer: customerId,
-    status: 'all',
+    status: 'active',
     expand: ['data.default_payment_method'],
   });
 
